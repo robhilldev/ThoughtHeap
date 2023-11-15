63bca34f04a52e52aa493c86915b80f776327d1d
Rob Hill <robert.hill731@gmail.com>
2023-11-14 23:41:27 -0500

    - use global key array - set each remove button id uniquely - add event listener to every remove button

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
+let keys = Object.keys(localStorage);
+
 window.onload = function() {
   document.getElementById("text-to-add").value = "";
 
-  let keys = Object.keys(localStorage);
   keys.sort();
   instantiateList(keys);
 
   let addButton = document.getElementById("form-button");
-  addButton.addEventListener("click", addListItem.bind(keys));
+  addButton.addEventListener("click", addListItem);
 }
 
 // create an instance of the list with items from localstorage
@@ -16,47 +17,49 @@ function instantiateList(keys) {
     let currentItem = localStorage.getItem(key);
     currentLi.innerHTML = currentItem;
     currentLi.id = key;
-    currentLi.insertAdjacentHTML(
-      "beforeend",
-      " <button id='remove-button' onclick='removeListItem(event)'>-</button>"
-    );
     document.getElementById("list").appendChild(currentLi);
+    addRemoveButton(currentLi, key);
   }
 }
 
-// add item to page
+// add item to page, localstorage, and add key to array
 function addListItem() {
   event.preventDefault();
   let newLi = document.createElement("li");
   let textToAdd = document.getElementById("text-to-add");
   
-  // add item to local storage
   if (textToAdd.value) {
     let nextKey = String(
-      Number(this.length) === 0 ? 1 : Number(this[this.length - 1]) + 1
+      Number(keys.length) === 0 ? 1 : Number(keys[keys.length - 1]) + 1
     );
-    localStorage.setItem(
-      nextKey,
-      textToAdd.value
-    );
-    this.push(nextKey);
+    // add item to local storage and key array
+    localStorage.setItem(nextKey, textToAdd.value);
+    keys.push(nextKey);
 
     // add item to page with an id and remove button
     newLi.innerHTML = textToAdd.value;
     newLi.id = nextKey;
-    newLi.insertAdjacentHTML(
-      "beforeend",
-      " <button id='remove-button' onclick='removeListItem(event)'>-</button>"
-    );
     document.getElementById("list").appendChild(newLi);
+    addRemoveButton(newLi, nextKey);
     // clear input box
     textToAdd.value= "";
   }
 }
 
-// remove item from page and localstorage
+// add remove button to list item with event listener
+function addRemoveButton(element, buttonNumber) {
+  element.insertAdjacentHTML(
+    "beforeend",
+    `<button id='remove-button-${buttonNumber}' class='remove'>-</button>`
+  );
+  let removeButton = document.getElementById(`remove-button-${buttonNumber}`);
+  removeButton.addEventListener("click", removeListItem);
+}
+
+// remove item from page, localstorage, and remove key array
 function removeListItem(e) {
   let listItem = e.target.parentElement;
   localStorage.removeItem(listItem.id);
+  keys = keys.filter((key) => key !== listItem.id);
   listItem.remove();
 }

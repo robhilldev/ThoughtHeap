1acc3102fae237b0e24f7ad3832a107df90edba3
Rob Hill <robert.hill731@gmail.com>
2023-12-18 13:58:21 -0500

    close title edit box upon clicking list select menu

--- Interaction.js
+++ Interaction.js
@@ -1,16 +1,24 @@
 // open or close the list selection menu when clicking its button
 function toggleListMenu() {
   const menu = document.getElementById("list-select-content");
-  const titleRightEdge = document.getElementsByTagName("header")[0]
+  const titleRightEdge = document
+    .getElementsByTagName("header")[0]
     .children[0].getBoundingClientRect().right;
-  const menuButtonLeftEdge = document.getElementsByTagName("header")[0]
+  const menuButtonLeftEdge = document
+    .getElementsByTagName("header")[0]
     .children[1].getBoundingClientRect().left;
 
   // before opening menu determine its size and position
   if (!menu.classList.contains("visible")) {
     menu.style.width = `${titleRightEdge - menuButtonLeftEdge}px`;
+
+    // and close an open title edit box before opening menu
+    const headerChild = document.getElementById("header").firstElementChild;
+    if (headerChild.classList.contains("edit-input-box")) {
+      document.getElementById("discard-edit-button-header").click();
+    }
   }
-  
+
   menu.classList.toggle("visible");
 }
 
